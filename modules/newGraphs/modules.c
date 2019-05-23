dea9730c2233014af82255a4391cd6753f89c966
Michalis Tatas <tatas.michalis@gmail.com>
2019-05-23 12:32:19 +0300

    ta space tou fwtiadh komple

--- modules/newGraphs/modules.c
+++ modules/newGraphs/modules.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
-
+#include <string.h>
 #include "graph.h"
 
 UGGraph* UGCreate()
 {
     UGGraph* graph = malloc(sizeof(UGGraph));
+    
     graph->hash = HTCreate();
     graph->Size = 0;
+    
     return graph;
 }
 
@@ -30,5 +32,157 @@ void UGRemoveVertex(UGGraph* graph, char* vertex)
 
 void UGAddEdge(UGGraph* graph, char* vertex1, char* vertex2)
 {
-     HTGet();
+    HTItem temp;
+    HTItem adjacentlist;
+    int index;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    HTNode* adjacentnode;
+
+
+
+
+
+    
+
+    //for the first vertex
+    //get his adjacent list and add at the end a pointer to vertex 2
+
+    index = HashFunction(vertex2, graph->hash->maxSize);
+    HTGet(graph->hash, vertex1, &adjacentlist);
+    temp = adjacentlist;                        //dont kow if necessary
+    while(temp->next != NULL)
+        temp = temp->next;
+    temp->next = malloc(sizeof(HTItem));
+    temp->next->next = NULL;
+
+    adjacentnode = graph->hash->array[index];       //find the vertex in the list of the array position since its a hash table
+    while(strcmp(adjacentnode->key, vertex2) != 0)
+        adjacentnode = adjacentnode->next;
+
+    temp->next->adjacentNode = adjacentnode;
+
+    //for the first vertex
+    //get his adjacent list and add at the end a pointer to vertex 2
+
+    index = HashFunction(vertex1, graph->hash->maxSize);
+    HTGet(graph->hash, vertex2, &adjacentlist);
+    temp = adjacentlist;
+    while(temp->next != NULL)
+        temp = temp->next;
+    temp->next = malloc(sizeof(HTItem));
+    temp->next->next = NULL;
+
+    adjacentnode = graph->hash->array[index];       //find the vertex in the list of the array position since its a hash table
+    while(strcmp(adjacentnode->key, vertex1) != 0)
+        adjacentnode = adjacentnode->next;
+
+    temp->next->adjacentNode = adjacentnode;
 }
+
+// void UGRemoveEdge(UGGraph* graph, char* vertex1, char* vertex2)
+// {
+//     HTItem temp;
+
+//     HTGet(graph->hash, vertex1, temp);
+//     if(strcmp(temp->adjacentNode->item, vertex2) == 0)
+//         if(temp->next == NULL)
+//             temp = NULL;
+//         else 
+// }
+
+HTItem UGGetAdjacent(UGGraph* graph, char* vertex)
+{
+    HTItem temp = malloc(sizeof(HTItem));
+
+    HTGet(graph->hash, vertex, &temp);
+
+    return temp;
+}
+
+void UGDestroy(UGGraph* graph)
+{
+    HTDestroy(graph->hash);
+    free(graph);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- newex2/main.c
+++ newex2/main.c
@@ -3,9 +3,27 @@
 
 int main(void)
 {
-    printf("SDFG");
     UGGraph* graph = UGCreate();
     graph->Size = 20;
-    
+    graph->hash->maxSize = 20;
+
+    UGAddVertex(graph, "foo");
+    UGAddVertex(graph, "goo");
+    UGAddVertex(graph, "hoo");
+    UGAddVertex(graph, "joo");
+    UGAddVertex(graph, "koo");
+    UGAddVertex(graph, "loo");
+
+    printf("number of vertexes is : %d \n", HTSize(graph->hash));
+
+    UGRemoveVertex(graph, "goo");
+
+    printf("number of vertexes after deletion is : %d \n", HTSize(graph->hash));
+
+    UGAddEdge(graph, "foo", "joo");
+
+    HTItem temp = UGGetAdjacent(graph, "foo");
+    if(temp->adjacentNode == NULL)
+        printf("POUTSOS");
     return 0;
 }
